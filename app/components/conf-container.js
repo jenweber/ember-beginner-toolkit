8a4e77747afe5262b6054f2791261986799a7b6a
Jen Weber <j@jenweber.me>
2017-10-15 14:00:14 -0400

    createRecord example

--- app/components/conf-container.js
+++ app/components/conf-container.js
@@ -1,4 +1,13 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+  createNewConference() {
+    let conference = {
+      name: "Boston Code Camp",
+      description: "A free developer conference hosted twice a year"
+    }
+    this.get('store').createRecord('conference', conference)
+  }
+  
 });
