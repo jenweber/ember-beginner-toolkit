0c24c7c73364497033b56257f45e73bea82a8e22
Jen Weber <j@jenweber.me>
2017-10-15 12:14:24 -0400

    define conference model

--- app/models/conference.js
+++ app/models/conference.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  name: DS.attr('string'),
+  date: DS.attr('date'),
+  description: DS.attr('string'),
+  attendees: DS.attr('number'),
+  registrationOpen: DS.attr('boolean')
 });
